1ee6c749636d711f74c2ca776d87d679eaf065a2
naveenmnk25 <97008354+naveenmnk25@users.noreply.github.com>
2023-03-22 19:23:42 +0530

    Changes

--- RoomManagement/RoomManagement/ViewModels/AdvanceViewModel.cs
+++ RoomManagement/RoomManagement/ViewModels/AdvanceViewModel.cs
@@ -14,13 +14,27 @@ namespace RoomManagement.ViewModels
 		public List<GetAdvance> Staying { get; set; }
 		public List<GetAdvance> vecate { get; set; }
 
-        public AdvanceViewModel GetModel()
+        public int TotalDetected { get; set; }
+        public int RemTotalToGive { get; set; }
+        public int TotalReFund { get; set; }
+
+		public int TotalDetected1 { get; set; }
+		public int RemTotalToGive1 { get; set; }
+		public List<AmountDetail> AmountDetail { get; set; }
+		public AdvanceViewModel GetModel()
 		{
             var productTreeString = _context.QueryResult.FromSqlRaw("Execute dbo.GetAdvance").ToList();
             this.GetAdvance = Newtonsoft.Json.JsonConvert.DeserializeObject<List<GetAdvance>>(productTreeString[0].JsonResult);
 			this.Staying = this.GetAdvance.Where(x => x.IsVecate == false).ToList();
 			this.vecate = this.GetAdvance.Where(x => x.IsVecate == true).ToList();
-            return this;
+
+			this.TotalDetected = this.GetAdvance.Sum(x => x.DetectedAdvAmt).Value;
+			this.RemTotalToGive = this.GetAdvance.Sum(x => x.RemAmtFromAd).Value;
+			this.TotalReFund = this.GetAdvance.Sum(x => x.AmountReFund).Value;
+			this.AmountDetail = _context.AmountDetails.ToList();
+			this.TotalDetected1 = this.AmountDetail.First().DetectedAmt.Value;
+			this.RemTotalToGive1 = 60000 - this.TotalDetected1;
+			return this;
 		}
     }
 }
--- SQL/SP/GetAdvance.sql
+++ SQL/SP/GetAdvance.sql
@@ -10,9 +10,12 @@ Begin
 			,AdvanceCalId = Ac.Id
 			,AmountGiven = Ac.AmountGiven
 			,AmountReFund = Ac.AmountReFund
-			,RemAmtFromAd = Ac.AmountGiven - Ac.AmountReFund - Ac.DetectedAmt
+			,RemAmtFromAd = case when (Ac.AmountGiven - Ac.AmountReFund - (Ad.DetectedAmt / 7)) <= 0 Then 0 Else (Ac.AmountGiven - Ac.AmountReFund - (Ad.DetectedAmt / 8)) End
 			,DetectedAmt  = Ac.DetectedAmt 
-			,DetectedAdvAmt  = Ad.DetectedAmt / 8
+			,DetectedAdvAmt  = 
+			Case When AmountGiven = 0 Then ((Ad.DetectedAmt / 7)/DATEDIFF(Month, '2022/06/25', GETDATE()))* DATEDIFF(Month, m.JoinDate, m.VecateDate)
+				When (Ac.AmountGiven - Ac.AmountReFund) = 0 Then 0
+			  Else ((Ad.DetectedAmt / 7)/DATEDIFF(Month, '2022/06/25', GETDATE()))* DATEDIFF(Month, m.JoinDate, m.VecateDate) End
 			,IsVecate = m.IsVecate
 		From Member(Nolock) m
 		Left Join AdvanceCal(Nolock) Ac
